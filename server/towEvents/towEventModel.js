f26a8ae37fa4ff1141f16c161e47b2f64d96fcd7
JustinTRoss <ross.justin.t@gmail.com>
2016-07-11 16:05:47 -0700

    Add timestamp to schemas, create models, and add export clause

--- server/towEvents/towEventModel.js
+++ server/towEvents/towEventModel.js
@@ -2,7 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var towEventSchema = new Schema({
-  licensePlate: String,
-  fromPhone: String,
-  toPhone: String
+  licensePlate: {type: String, required: true},
+  fromPhone: {type: String, required: false},
+  toPhone: {type: String, required: true},
+  type: {type: String, required: true} //should always either be warning or inAction
 });
+towEventSchema.set('timestamps', true);
+
+var TowEvent = mongoose.model('TowEvent', 'Schema');
+
+module.exports = TowEvent
--- server/users/userModel.js
+++ server/users/userModel.js
@@ -2,8 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-  name: String,
-  phone: String,
-  licensePlate: String,
-  towAddress: String
+  name: {type: String, required: true},
+  phone: {type: String, required: true},
+  licensePlate: {type: String, required: true},
+  towAddress: {type: String, required: true},
+  incidences: {type: Number, default: 0}
 });
+userSchema.set('timestamps', true);
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
