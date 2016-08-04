618bf438c01d6f05e02b9ecb71393dc8924dc957
Justin Ross <Ross.Justin.T@gmail.com>
2016-08-03 21:38:01 -0700

    Include body-parser, morgan, and cors middlewares

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, './client')));
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
